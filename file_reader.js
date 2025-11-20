2f8f28c9668bebe3fa1182cc4de716b0209f9abb
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2025-11-19 20:28:33 -0800

    chore: finish

--- file_reader.js
+++ file_reader.js
@@ -1,10 +1,53 @@
 const fs = require("fs").promises;
 
+const charset = "utf-8";
+
+const firstnameFileName = "firstname.txt";
+const lastnameFileName = "lastname.txt";
+const ageFileName = "age.txt";
+const hobbiesFileName = "hobbies.txt";
+
 // THEN-CATCH SOLUTION BELOW THIS LINE
-
-
-
-
-
+{
+  let firstname, lastname, age, hobbies;
+  fs.readFile(firstnameFileName, charset)
+    .then((data) => {
+      firstname = data;
+      return fs.readFile(lastnameFileName, charset);
+    })
+    .then((data) => {
+      lastname = data;
+      return fs.readFile(ageFileName, charset);
+    })
+    .then((data) => {
+      age = data;
+      return fs.readFile(hobbiesFileName, charset);
+    })
+    .then((data) => {
+      hobbies = JSON.parse(data);
+      console.log(
+        `${firstname} ${lastname} is ${age} years old and his hobbies are ${hobbies[0]} and ${hobbies[1]}`
+      );
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
 
 // ASYNC/AWAIT SOLUTION BELOW THIS LINE
+const readFiles = async () => {
+  try {
+    const firstname = await fs.readFile(firstnameFileName, charset);
+    const lastname = await fs.readFile(lastnameFileName, charset);
+    const age = await fs.readFile(ageFileName, charset);
+    const hobbiesData = await fs.readFile(hobbiesFileName, charset);
+    const hobbies = JSON.parse(hobbiesData);
+
+    console.log(
+      `${firstname} ${lastname} is ${age} years old and his hobbies are ${hobbies[0]} and ${hobbies[1]}`
+    );
+  } catch (err) {
+    console.error(err);
+  }
+};
+readFiles();
